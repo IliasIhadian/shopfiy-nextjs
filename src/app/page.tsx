2c503f796f97ec0f499395dd45258d59115767ef
Ilias Ihadian <i.ihadian@gmail.com>
2024-10-08 08:27:46 +0200

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-3">
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
